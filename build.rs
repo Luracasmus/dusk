390d643d3d83a63b54b4400b68d34d8ea19cf669
Lura <77991691+Luracasmus@users.noreply.github.com>
2023-09-08 20:09:07 +0200

    Update build.rs

--- build.rs
+++ build.rs
@@ -1,15 +1,16 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use std::env::var_os;
-use embed_manifest::{embed_manifest, new_manifest, manifest::{HeapType, MaxVersionTested, SupportedOS::Windows10}};
+
+use embed_manifest::{embed_manifest, new_manifest, manifest::{MaxVersionTested, SupportedOS::Windows10}};
 
 fn main() {
 	if var_os("CARGO_CFG_WINDOWS").is_some() {
 		embed_manifest(new_manifest("Dusk")
 			.supported_os(Windows10..=Windows10)
 			.max_version_tested(MaxVersionTested::Windows11Version22H2)
-			.heap_type(HeapType::SegmentHeap))
-			.expect("unable to embed manifest file");
+			//.heap_type(HeapType::SegmentHeap) // This annihilates FFmpeg performance for some reason
+		).expect("Unable to embed manifest file");
 	}
 
 	println!("cargo:rerun-if-changed=build.rs");
