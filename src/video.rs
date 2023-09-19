445aba28ac0b76a9bab47e6c3ec02163a4b8ba49
Lura <77991691+Luracasmus@users.noreply.github.com>
2023-09-19 21:54:55 +0200

    Update video.rs

--- src/video.rs
+++ src/video.rs
@@ -1,4 +1,4 @@
-use std::{cmp::Ordering, path::PathBuf, num::NonZeroU32};
+use std::{cmp::Ordering, path::PathBuf, num::NonZeroU16, ops::RangeInclusive};
 
 use ffmpeg_sidecar::{child::FfmpegChild, event::OutputVideoFrame, command::FfmpegCommand};
 use tiny_skia::{Pixmap, IntSize};
@@ -17,17 +17,14 @@ pub enum Drag {
 /// Contains metadata about a specific video as well as the `FFmpeg` instance, iterator and functions required to load frames
 pub struct Video {
 	pub frame: Pixmap,
-	pub start: u32,
 	pub x: f32,
 	pub y: f32,
-	pub sx: f32,
-	pub sy: f32,
-	pub scaled: bool,
+	pub scale: Option<(f32, f32)>,
 	pub drag: Drag,
-	width: NonZeroU32,
-	height: NonZeroU32,
-	pub visible: bool,
+	in_width: NonZeroU16,
+	in_height: NonZeroU16,
 	pub ffmpeg: FfmpegChild,
+	pub duration: RangeInclusive<f32>,
 
 	path: PathBuf,
 	frame_num: u32,
@@ -37,12 +34,7 @@ pub struct Video {
 
 impl Video {
 	/// Creates a new [`Video`] from a path and calls `load()` on it's first frame
-	pub fn new(path: PathBuf) -> Self {
-		let full_path = path.canonicalize().unwrap();
-
-		assert!(path.try_exists().unwrap(), "Path does not exist: {full_path:?}");
-		assert!(path.is_file(), "Path is not a valid file: {full_path:?}");
-
+	pub fn new(path: PathBuf) -> Option<Self> {
 		let mut ffmpeg = FfmpegCommand::new()
 			.hide_banner()
 			.create_no_window()
@@ -54,35 +46,33 @@ impl Video {
 			.pix_fmt("rgba")
 			.no_overwrite()
 			.pipe_stdout()
-			//.output("-")
 			.spawn().unwrap();
 
 		let mut iter = ffmpeg.iter().unwrap().filter_frames();
 
-		let frame = iter.next().expect("FFmpeg encountered an error. Are you sure all paths are videos?");
+		let frame = iter.next()?;
 
 		let fps = {
-			let next_frame = iter.next().unwrap();
-
-			(100.0 / (next_frame.timestamp - frame.timestamp)).round() * 0.01
+			if let Some(next_frame) = iter.next() {
+				(100.0 / (next_frame.timestamp - frame.timestamp)).round() * 0.01
+			} else {
+				0.0 // "Video" is a still image
+			}
 		};
 
 		println!("{path:?} fps: {fps}");
 
 		let mut video = Self {
-			frame: Pixmap::from_vec(frame.data, IntSize::from_wh(frame.width, frame.height).unwrap()).unwrap(),
+			frame: Pixmap::from_vec(frame.data, IntSize::from_wh(frame.width, frame.height)?)?,
 			path,
 			frame_num: 0,
 			fps,
-			start: 0,
+			duration: 0.0..=f32::MAX,
 			x: 0.0,
 			y: 0.0,
-			sx: 1.0,
-			sy: 1.0,
-			width: NonZeroU32::new(frame.width).unwrap(),
-			height: NonZeroU32::new(frame.height).unwrap(),
-			visible: true,
-			scaled: false,
+			scale: None,
+			in_width: NonZeroU16::new(frame.width as u16)?,
+			in_height: NonZeroU16::new(frame.height as u16)?,
 			drag: Drag::None,
 			ffmpeg,
 			iter: Box::new(iter)
@@ -90,14 +80,14 @@ impl Video {
 
 		video.load(0.0); // Undo the iterator advancements above
 
-		video
+		Some(video)
 	}
 
 	/// Requests for the [`Video`] to load a new frame into it's `frame` field
 	///
 	/// * If the frame has the same timestamp as the last frame, nothing is changed
 	/// * If it has a larger timestamp, `Video.iter` will advance until it reaches that timestamp
-	/// * If it has a smaller timestamp, the `reload()` is called on the [`Video`] and it's `ffmpeg`, `iter` and `frame` are replaced by ones starting at the requested timestamp
+	/// * If it has a smaller timestamp, `reload()` is called on the [`Video`] and it's `ffmpeg`, `iter` and `frame` are replaced by ones starting at the requested timestamp
 	pub fn load(&mut self, timestamp: f32) {
 		let num = (timestamp * self.fps).round() as u32;
 
@@ -136,15 +126,16 @@ impl Video {
 		}
 	}
 
-	/// Multiplies the [`Video`]'s `width` and `height` fields by it's `sx` and `sy` scale factor fields, which are then reset to `1.0`
+	/// Multiplies the [`Video`]'s `in_width` and `in_height` fields by it's `scale` field, which is then set to None
 	///
 	/// It also advances the `iter` field by `1` frame and then `-1` frame in order to force the video to `reload()` and apply the new `width` and `height`
 	pub fn resize(&mut self) {
-		self.width = NonZeroU32::new((self.width.get() as f32 * self.sx).round() as u32).unwrap();
-		self.height = NonZeroU32::new((self.height.get() as f32 * self.sy).round() as u32).unwrap(); // Crashes if scaling video too small
+		let (sx, sy) = self.scale.expect("Resized Video with no Scale");
 
-		self.sx = 1.0;
-		self.sy = 1.0;
+		self.in_width = NonZeroU16::new(((self.in_width.get() as f32 * sx).round() as u16).max(1)).unwrap();
+		self.in_height = NonZeroU16::new(((self.in_height.get() as f32 * sy).round() as u16).max(1)).unwrap();
+
+		self.scale = None;
 
 		self.load((self.frame_num + 1) as f32 / self.fps);
 		self.load((self.frame_num - 1) as f32 / self.fps);
@@ -152,7 +143,7 @@ impl Video {
 
 	/// Replaces the [`Video`]'s `ffmpeg` and `iter` fields with new ones starting from `Video.timestamp`
 	///
-	/// This also applies changes from the `width` and `height` fields
+	/// This also applies changes from the `in_width` and `in_height` fields
 	fn reload(&mut self) {
 		let _ = self.ffmpeg.quit(); // Probably not good but .unwrap() sometimes panics
 
@@ -166,10 +157,9 @@ impl Video {
 			.input(self.path.to_str().unwrap())
 			.format("rawvideo")
 			.pix_fmt("rgba")
-			.size(self.width.get(), self.height.get())
+			.size(self.in_width.get() as u32, self.in_height.get() as u32)
 			.no_overwrite()
 			.pipe_stdout()
-			//.output("-")
 			.spawn().unwrap();
 
 		self.iter = Box::new(self.ffmpeg.iter().unwrap().filter_frames());
